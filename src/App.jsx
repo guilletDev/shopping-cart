e401d99e44ee433c0a237bb7b180ce384f7fdca6
guilletdev <guilletdev@gmail.com>
2024-10-17 19:24:22 -0300

    Mostramos productos y agregamos estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import {products} from './mocks/products.json'
+import { Products } from "./components/Products"
 function App() {
  
 
   return (
     <>
       <h1>Shopping cart 🛒</h1>
+      <Products products={products} />
     </>
   )
 }
